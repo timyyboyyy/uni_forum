ec0b1fa45f0a763cac65c85eab4665bdfffa0abc
timyyboyyy <tim.oliver.platzek@gmx.de>
2025-03-26 13:06:55 +0100

    input admin

--- docker/mysql/input.sql
+++ docker/mysql/input.sql
@@ -5,3 +5,8 @@ VALUES
 ('Announcements', 'Official announcements and updates from the forum team.'),
 ('Feedback', 'Share your feedback and suggestions for the forum.');
 
+
+-- Anlegen eine Admin Kontos    --Einlogdaten: Username: admin  Password:admin
+INSERT INTO users(password_hash, email, username, rules_ID) 
+VALUES
+('$2y$10$C680Di96Al4GKmshXxAgtOm76w/ko9obeAZyj0PN5ILPkFG1I9iu.','admin@admin.de','admin',1);
